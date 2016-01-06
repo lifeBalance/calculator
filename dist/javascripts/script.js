7404f6644cf4064cbbc6fc0fa29f19a7bf795195
Javier Rodriguez <javierorodriguez@gmail.com>
2016-01-06 22:33:58 +0300

    Add function for building up operands

--- dist/javascripts/script.js
+++ dist/javascripts/script.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   var display = 0;
   var str;
 
-  // Set the display to '0'.
+  // Set the display to '0' at the beginning.
   // Comment following line to test 'AC' button
-  // initialize();
+  initialize();
 
   // Also triggered when the 'All Clear' is clicked.
   function initialize() {
@@ -12,9 +12,28 @@ $(document).ready(function () {
     $('#display').val(str);
   }
 
+  // Building up the operands
+  function buildOperand(num) {
+    if (str[0] === '0' && num === '0') {
+      return;
+    } else if (str[0] === '0' && num !== '0' && str.length === 1){
+      str = '';
+      str += num.toString();
+      $('#display').val(str);
+    } else {
+      str += num.toString();
+      $('#display').val(str);
+    }
+  }
+
+  // Listening for 'click' events.
   $('#calculator').on('click', 'a', function (event) {
     if ($(this).data('op') === 'ac') {
       initialize();
+    } else if ($(this).data('key') !== false) {
+      buildOperand($(this).data('key'));
     }
+
+    console.log(str);
   });
 });
