1b3957160a25e3d1e8020b42697aa18c44b23abe
Javier Rodriguez <javierorodriguez@gmail.com>
2016-01-06 22:29:06 +0300

    Add 'All Clear' (clear display) functionality

--- dist/javascripts/script.js
+++ dist/javascripts/script.js
@@ -1,5 +1,20 @@
-$('.grid').masonry({
-  // options...
-  itemSelector: '.grid-item',
-  columnWidth: 200
+$(document).ready(function () {
+  var display = 0;
+  var str;
+
+  // Set the display to '0'.
+  // Comment following line to test 'AC' button
+  // initialize();
+
+  // Also triggered when the 'All Clear' is clicked.
+  function initialize() {
+    str = '0';
+    $('#display').val(str);
+  }
+
+  $('#calculator').on('click', 'a', function (event) {
+    if ($(this).data('op') === 'ac') {
+      initialize();
+    }
+  });
 });
