fa38a957a78fdca922c904eb584a473cb3b37eb8
Javier Rodriguez <javierorodriguez@gmail.com>
2016-01-22 23:37:50 +0300

    Huge refactoring

--- dist/javascripts/script.js
+++ dist/javascripts/script.js
@@ -1,93 +1,192 @@
 $(document).ready(function () {
-  var display = 0;
-  var signState = '';
-  var decimalState = false;
-  var str;
+  // Some states
+  var signState;
+
+  // Once decimalState is enabled, the user cannot
+  // introduce more decimal points.
+  var decimalState;
+  var operationState;
+
+  var buffer;
+
+  var firstOperand;
+  var secondOperand;
+  var operator;
 
   // Set the display to '0' at the beginning.
-  // Comment following line to test 'AC' button
+  // (Comment following line to test 'AC' button)
   initialize();
 
   // Also triggered when the 'All Clear' is clicked.
-  function initialize() {
-    str = '0';
+  function initialize(refreshDisplay) {
+    console.clear();
+    buffer = '0';
+
     decimalState = false;
-    $('#display').val(str);
+    operationState = false;
+    signState = '';
+    operator = '';
+
+    return refresh(buffer);
   }
 
-  // Building up the operands
-  function buildOperand(num) {
-    if (str[0] === '0' && num === '0') {
-      return;
-    } else if (str[0] === '0' && num !== '0' && str.length === 1){
-      str = '';
-      str += num.toString();
-      display = Number(str);
-      $('#display').val(str);
-    } else {
-      str += num.toString();
-      display = Number(str);
-      $('#display').val(str);
+  // `key` is an object, key.num or key.op
+  function calculate(key) {
+    if (key.num) {
+      buildOperand(key, refresh);
+    }
+
+    // When the AC key is pressed, no matter what, initialize
+    if (key.op === 'ac') {
+      initialize(refresh);
+    }
+
+    // We have pressed an operator key, and operationState is false,
+    // that means is the first time we press an operator key.
+    if (key.op && key.op !== 'ac' && operationState === false) {
+      // If the first operator is 'equals' we just return.
+      if (key.op === 'equals') {
+        return;
+      }
+
+      // Any other operator:
+      // The value of buffer is stored(as a Number) in the first operand.
+      firstOperand = Number(buffer);
+      operationState = true;
+      // The operator is also stored
+      operator = key.op;
+
+      console.log('buffer now is:', buffer);
+      console.log('operating?', operationState);
+      // We flush the buffer to make place for the second operand,
+      // but we don't refresh the display. It will be refreshed
+      // when the second operand is being introduced.
+      buffer = '0';
+      console.log('buffer now is:', buffer);
+
+  // We have pressed an operator key, and operationState is true,
+  // meaning it's the second time we press an operator key.
+  } else if (key.op && key.op !== 'ac' && operationState === true) {
+      operationState = false;
+      secondOperand = Number(buffer);
+      doTheMath(refresh);
     }
   }
 
-  // Add minus sign
-  function addMinus(cb) {
-    signState = '-';
-    str = signState + str;
-    cb();
+  function buildOperand(key, refreshDisplay) {
+    // When we have only 0 in the display.
+    if (buffer.length === 1 && buffer[0] === '0') {
+      // If user presses 0 or tries to change sign
+      // we just return.
+      if (key.num === 0 || key.num === 'sign') {
+        return;
+        // If user presses the decimal point
+      } else if (key.num === '.') {
+        console.log('buffer is', buffer);
+        addDot(refresh);
+      // If user presses another number
+      } else {
+        // Overwrite the first digit
+        buffer = key.num;
+        return refreshDisplay(buffer);
+      }
+    }
+
+    // If there's more than 1 digit in the display
+    if (buffer.length >= 1){
+      if (key.num === '.') {
+        addDot(refresh);
+      } else if (key.num === 'sign') {
+        toggleSign(refresh);
+      } else {
+        // Append the digit(as a string) to the buffer string
+        buffer += key.num;
+        return refreshDisplay(buffer);
+      }
+    }
   }
 
-  // Remove minus sign
-  function removeMinus(cb) {
-    signState = '';
-    str = str.replace('-', '');
-    cb();
+  // Add decimal point
+  function addDot(refreshDisplay) {
+    console.log('buffer is', buffer);
+    // If decimalState is true we just return
+    if (decimalState) {
+      return;
+    } else {
+      // Add the decimal point.
+      buffer += '.';
+      // And toggle decimalState
+      decimalState = true;
+      return refreshDisplay(buffer);
+    }
   }
 
   // Toggle sign
-  function toggleSign() {
-    var arr = [];
+  function toggleSign(refreshDisplay) {
+    if (signState === '' && buffer.length === 1 && buffer[0] !== '0') {
+      signState = '-';
+      return refreshDisplay(buffer = signState + buffer);
+    } else if (signState === '' && buffer.length > 1){
+      signState = '-';
+      return refreshDisplay(buffer = signState + buffer);
+    } else if (signState === '-') {
+      signState = '';
+      buffer = buffer.replace('-', signState);
+      return refreshDisplay(buffer);
+    }
+  }
 
-    if (signState === '' && display > 0) {
-      addMinus(refresh);
+  // Doing the calculations
+  function doTheMath(refreshDisplay) {
+    switch (operator) {
+    case 'addition':
+      buffer = firstOperand + secondOperand;
+      firstOperand = buffer;
+      return refreshDisplay(buffer);
+    case 'substraction':
+      return refreshDisplay(firstOperand - secondOperand);
+    case 'multiplication':
+      return refreshDisplay(firstOperand * secondOperand);
+    case 'division':
+      return refreshDisplay(divide(firstOperand, secondOperand));
+    }
+  }
+
+  // Division
+  function divide(a, b) {
+    if(b === 0) {
+      return 0;
     } else {
-      removeMinus(refresh);
+      return a / b;
     }
   }
 
   // Refresh display
-  function refresh() {
+  function refresh(str) {
     $('#display').val(str);
-  }
-
-  // Add decimal point
-  function addDot(cb) {
-    if (decimalState) {
-      return;
-    } else {
-      decimalState = true;
-      str += '.';
-      cb();
-    }
   }
 
   // Listening for 'click' events.
   $('#calculator').on('click', 'a', function (event) {
     event.preventDefault();
-    if ($(this).data('op') === 'ac') {
-      initialize();
-    } else if ($(this).data('key') !== undefined) {
-      buildOperand($(this).data('key'));
-      console.log($(this).data('key'), $(this).data('op'));
-    } else if ($(this).data('op') === 'sign') {
-      console.log($(this).data('key'), $(this).data('op'));
-      toggleSign();
-    } else if ($(this).data('op') === 'dot') {
-      console.log($(this).data('key'), $(this).data('op'));
-      addDot(refresh);
-    }
+    // This object will store something like either:
+    // {num: "6"} or {op: "addition"}
+    var key = {}; // initialize to {} on each key-press
 
-    console.log(str);
+    // I found that using $(this).data('num') did NOT
+    // work when the pressed key was 0!!
+    // A bug in jQuery??
+    if ($(this).attr('data-num')) {
+      key.num = $(this).attr('data-num');
+      console.log('Number Key pressed: %s', key.num);
+      // console.log(typeof key.num);
+      // console.log(key);
+      // calculate(key);
+    } else if ($(this).attr('data-op')) {
+      key.op = $(this).attr('data-op');
+      console.log('Operator Key pressed: %s', key.op);
+      // console.log(key);
+    }
+    calculate(key);
   });
 });
