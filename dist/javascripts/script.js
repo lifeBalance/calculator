0a22709ef26c79b33e4a4267a3bc878a1973b3b6
Javier Rodriguez <javierorodriguez@gmail.com>
2016-01-24 21:54:00 +0300

    Add background and fonts

--- dist/javascripts/script.js
+++ dist/javascripts/script.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
 
   var buffer;
 
-  var firstOperand;
-  var secondOperand;
+  var operand;
+  // var secondOperand;
   var operator;
 
   // Set the display to '0' at the beginning.
@@ -24,143 +24,143 @@ $(document).ready(function () {
   // Also triggered when the 'All Clear' is clicked.
   function initialize(refreshDisplay) {
     console.clear();
-    buffer = '0';
+    operand = '0';
 
     decimalState = false;
     operationState = false;
     signState = '';
     operator = '';
 
-    return refresh(buffer);
+    return refresh(operand);
   }
 
   // `key` is an object, key.num or key.op
   function calculate(key) {
     if (key.num) {
       buildOperand(key, refresh);
-    }
-
-    // When the AC key is pressed, no matter what, initialize
-    if (key.op === 'ac') {
+    } else if (key.op === 'ac') {
+      // When the AC key is pressed, no matter operationState, initialize
       initialize(refresh);
-    }
+    } else { // We have pressed any other operator key (not AC)
+      console.log(operationState);
+      if(!operationState) { // operationState is false (it's the first time we press an operator key)
+        switch (key.op) {
+          case 'equals': // If the first operator is 'equals' we just return.
+          return;
 
-    // We have pressed an operator key, and operationState is false,
-    // that means is the first time we press an operator key.
-    if (key.op && key.op !== 'ac' && operationState === false) {
-      // If the first operator is 'equals' we just return.
-      if (key.op === 'equals') {
-        return;
-      } else if (key.op === 'percentage') {
-        percentage(Number(buffer), refresh);
+          case 'percentage': // If the first operator is 'equals' we just return.
+          percentage(Number(buffer), refresh);
+          break;
+
+          default:
+          buffer = Number(operand); // Store the operand in the buffer
+          operator = key.op; // Store the operator
+          operationState = true; // Change operationState to true
+
+          // The operand is set to '0' to leave space to the next one,
+          // but the display is not refreshed. As we start introducing
+          // the next operand the display will be refreshed.
+          operand = '0';
+        } // switch
+      } else { // operationState is true (it's the second time we press an operator key)
+        switch (key.op) {
+          case 'percentage':
+          percentage(buffer * Number(operand), refresh);
+          return;
+
+          case 'equals':
+          refresh(buffer = doTheMath(Number(operand), buffer, operator));
+          operationState = false;
+          break;
+
+          default:
+          refresh(buffer = doTheMath(Number(operand), buffer, operator));
+          operationState = false;
+          operator = key.op; // Updating the operator
+          operand = '0';
+        }
       }
-
-      // Any other operator:
-      // The value of buffer is stored(as a Number) in the first operand.
-      firstOperand = Number(buffer);
-      operationState = true;
-      // The operator is also stored
-      operator = key.op;
-
-      console.log('buffer now is:', buffer);
-      console.log('operating?', operationState);
-      // We flush the buffer to make place for the second operand,
-      // but we don't refresh the display. It will be refreshed
-      // when the second operand is being introduced.
-      buffer = '0';
-      console.log('buffer now is:', buffer);
-
-  // We have pressed an operator key, and operationState is true,
-  // meaning it's the second time we press an operator key.
-  } else if (key.op && key.op !== 'ac' && operationState === true) {
-    if (key.op === 'percentage') {
-      // firstOperand = Number(buffer);
-      // secondOperand = Number(buffer);
-      percentage(firstOperand * Number(buffer), refresh);
-      return;
     }
-      operationState = false;
-      secondOperand = Number(buffer);
-      doTheMath(refresh);
-    }
-  }
+  } // function calculate
 
   function buildOperand(key, refreshDisplay) {
     // When we have only 0 in the display.
-    if (buffer.length === 1 && buffer[0] === '0') {
+    if (operand.length === 1 && operand[0] === '0') {
       // If user presses 0 or tries to change sign
       // we just return.
       if (key.num === 0 || key.num === 'sign') {
         return;
         // If user presses the decimal point
       } else if (key.num === '.') {
-        console.log('buffer is', buffer);
         addDot(refresh);
       // If user presses another number
       } else {
         // Overwrite the first digit
-        buffer = key.num;
-        return refreshDisplay(buffer);
+        operand = key.num;
+        return refreshDisplay(operand);
       }
     }
 
     // If there's more than 1 digit in the display
-    if (buffer.length >= 1){
+    if (operand.length >= 1){
       if (key.num === '.') {
         addDot(refresh);
       } else if (key.num === 'sign') {
         toggleSign(refresh);
       } else {
         // Append the digit(as a string) to the buffer string
-        buffer += key.num;
-        return refreshDisplay(buffer);
+        operand += key.num;
+        return refreshDisplay(operand);
       }
     }
   }
 
   // Add decimal point
   function addDot(refreshDisplay) {
-    console.log('buffer is', buffer);
     // If decimalState is true we just return
     if (decimalState) {
       return;
     } else {
       // Add the decimal point.
-      buffer += '.';
+      operand += '.';
       // And toggle decimalState
       decimalState = true;
-      return refreshDisplay(buffer);
+      return refreshDisplay(operand);
     }
   }
 
   // Toggle sign
   function toggleSign(refreshDisplay) {
-    if (signState === '' && buffer.length === 1 && buffer[0] !== '0') {
+    if (signState === '' && operand.length === 1 && operand[0] !== '0') {
       signState = '-';
-      return refreshDisplay(buffer = signState + buffer);
-    } else if (signState === '' && buffer.length > 1){
+      return refreshDisplay(operand = signState + operand);
+    } else if (signState === '' && operand.length > 1){
       signState = '-';
-      return refreshDisplay(buffer = signState + buffer);
+      return refreshDisplay(operand = signState + operand);
     } else if (signState === '-') {
       signState = '';
-      buffer = buffer.replace('-', signState);
-      return refreshDisplay(buffer);
+      operand = operand.replace('-', signState);
+      return refreshDisplay(operand);
     }
   }
 
   // Doing the calculations
-  function doTheMath(refreshDisplay) {
+  function doTheMath(num1, num2, operator, refreshDisplay) {
     switch (operator) {
     case 'addition':
-      buffer = firstOperand + secondOperand;
-      firstOperand = buffer;
-      return refreshDisplay(buffer);
+      return num1 + num2;
+      // return refreshDisplay(num1 + num2);
     case 'substraction':
-      return refreshDisplay(firstOperand - secondOperand);
+      return num1 - num2;
+      // return refreshDisplay(num1 - num2);
     case 'multiplication':
-      return refreshDisplay(firstOperand * secondOperand);
+      return num1 * num2;
+      // return refreshDisplay(num1 * num2);
     case 'division':
-      return refreshDisplay(divide(firstOperand, secondOperand));
+      return divide(num1, num2);
+      // return refreshDisplay(divide(num1, num2));
+    case 'equals':
+      return buffer;
     case 'percentage':
       return refreshDisplay(percentage());
     }
@@ -180,8 +180,9 @@ $(document).ready(function () {
   }
 
   // Refresh display
-  function refresh(str) {
-    $('#display').val(str);
+  function refresh(num) {
+    $('#display').val(num);
+    return num;
   }
 
   // Listening for 'click' events.
